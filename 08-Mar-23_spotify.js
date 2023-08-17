c89c9d1fda23abb716530fc531406af741871baf
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-17 09:50:52 +0530

    Update 08-Mar-23_spotify.js

--- 08-Mar-23_spotify.js
+++ 08-Mar-23_spotify.js
@@ -20,6 +20,7 @@ const browserType = 'chromium'; // chrome
 const username = process.env.SPOTIFY_USERNAME;
 const password = process.env.SPOTIFY_PASSWORD;
 
+
 async function main() {
   //const browser = await playwright[browserType].launch({ headless: false });
 

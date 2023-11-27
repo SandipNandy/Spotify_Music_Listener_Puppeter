cb55b7291ebd1736ad39b32e2e51a66be03677b3
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-27 09:13:20 +0530

    Update 08-Mar-23_spotify.js

--- 08-Mar-23_spotify.js
+++ 08-Mar-23_spotify.js
@@ -32,7 +32,6 @@ async function main() {
   const browserWSEndpoint = 'ws://127.0.0.1:9222/devtools/browser/01f749a-f1be-45ac-b606-6e00abc417ee';
  
   const browser = await puppeteer.connect({ browserWSEndpoint });
- 
   const page = await browser.newPage()
  
   // compat the existing function so the existing code doesn't have to be changed

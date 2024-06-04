a0956b21a9137189d0cc2603d0762e0bc7800845
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-06-05 00:08:12 +0530

    Update 08-Mar-23_spotify.js

--- 08-Mar-23_spotify.js
+++ 08-Mar-23_spotify.js
@@ -22,6 +22,7 @@ const puppeteer = require('puppeteer');
 const browserType = 'chromium';
 
 const username = process.env.SPOTIFY_USERNAME;
+
 const password = process.env.SPOTIFY_PASSWORD;
 async function main() {
   //const browser = await playwright[browserType].launch({ headless: false });

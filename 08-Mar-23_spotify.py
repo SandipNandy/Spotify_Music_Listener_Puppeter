b41b1b6ba0c205cded1621abf45a7549ac40249b
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2024-10-23 21:49:12 +0530

    Update 08-Mar-23_spotify.py

--- 08-Mar-23_spotify.py
+++ 08-Mar-23_spotify.py
@@ -20,6 +20,7 @@ from instapy_chromedriver import binary_path
 
 
 class SpotifyListenerBot:
+    
     def __init__(self, username, password):
         """
         Initialise the session on spotify and try to

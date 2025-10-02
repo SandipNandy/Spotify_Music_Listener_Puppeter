6e9eab0f3130e796c933bb5517c90b689a19db52
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-10-02 22:34:16 +0530

    Update 08-Mar-23_spotify.js

--- 08-Mar-23_spotify.js
+++ 08-Mar-23_spotify.js
@@ -15,6 +15,7 @@ const puppeteer = require('puppeteer');
 // chrome
 
 const browserType = 'chromium';
+
 const username = process.env.SPOTIFY_USERNAME;
 const password = process.env.SPOTIFY_PASSWORD;
 

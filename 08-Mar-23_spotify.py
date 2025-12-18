38f725c73074310b4a4c316d280e6b583e2cf49e
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2025-12-18 12:06:27 +0530

    Update 08-Mar-23_spotify.py

--- 08-Mar-23_spotify.py
+++ 08-Mar-23_spotify.py
@@ -39,7 +39,6 @@ class SpotifyListenerBot:
         is_logged_in = self.login(username, password)
         
         if not is_logged_in:
-            
             raise Exception("Could not login with given credentials.")
 
     def login(self, username, password):
